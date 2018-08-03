eb6255fa7ae9a2050aa81985735cee249f248257
Yvonne Panganiban <panganibanyvonne.yp@gmail.com>
2018-08-03 16:51:07 -0400

    test

--- basic.js
+++ basic.js
@@ -5,3 +5,17 @@
 	} else {
 	    a = 'yes';
 	}
+
+	function myCheck(age, country) {
+	    var response;
+	    if (country == "USA") { 
+	    	response = age > 21 ? true : false; 
+	    } else if(country == "Canada") {
+	    	response = age > 18 ? true : false;
+	    } else {
+	    	response = false;
+	    }
+	    return response;
+	}
+
+	var d = (b > c) ? true : 'false';
